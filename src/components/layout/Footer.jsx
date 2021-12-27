32c85e000feb8edbfd90024148c138f660c62575
Martin.Smith <martin.smifff@gmail.com>
2021-12-27 10:22:05 +0000

    Add a spinner

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 function Footer() {
-  const footerYear = new Date().getFullYear
+  const footerYear = new Date().getFullYear()
 
   return (
     <footer className='footer p-10 bg-gray-700 text-primary-content footer-center'>
