e90eefd7e1667a57a2de55b8f5f212d14617c0dd
Martin.Smith <martin.smifff@gmail.com>
2021-12-28 14:32:32 +0000

    Finished

--- src/App.js
+++ src/App.js
@@ -4,25 +4,35 @@ import About from './pages/About'
 import NotFound from './pages/NotFound'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
+import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
+import Alert from './components/layout/Alert'
+import User from './pages/User'
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
+    <AlertProvider>
+      <GithubProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-        </main>
+            <main className='container mx-auto px-3 pb-12'>
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/user/:login' element={<User />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
 
-        <Footer />
-      </div>
-    </Router>
+            <Footer />
+          </div>
+        </Router>
+      </GithubProvider>
+    </AlertProvider>
   )
 }
 
